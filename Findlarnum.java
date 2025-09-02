0f20abbb6fe0c340dfde3e4c6ccec4c8876e7ac6
dharani-ks <dharanikrishnan035@gmail.com>
2025-09-02 20:41:58 +0530

    Add FindLargest Number

---
+++ Findlarnum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Findlarnum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter 1st number:");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter 2nd number:");
+        int num2 = sc.nextInt();
+
+        System.out.print("Enter 3rd number:");
+        int num3 = sc.nextInt();
+
+        if (num1> num2 && num1 > num3) {
+            System.out.print(+ num1 +"is largest  than "+num2+ "and"+num3);
+            
+        }else if(num2 > num1 && num2 > num3){
+            System.out.print(+ num2 +"is largest  than "+num1+ "and"+num3);
+        } 
+        else {
+            System.out.print(+ num3 +"is largest  than "+num1+ "and"+num2);
+  
+                }
+        sc.close();
+    }
+}
