dd8381f962c1614c5c016ec46c0581b9a513d528
dharani-ks <dharanikrishnan035@gmail.com>
2025-09-03 20:20:26 +0530

    My latest changes

--- CelToFaren.java
+++ CelToFaren.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class CelToFaren {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a celsius value:");
-        int cel = sc.nextInt();
-        int far = (cel*9/5) + 32;
-        
-        System.out.print("fahrenheit of "+cel+" is "+far);
-        sc.close();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a celsius value:");
+            int cel = sc.nextInt();
+            int far = (cel*9/5) + 32;
+            
+            System.out.print("fahrenheit of "+cel+" is "+far);
+        }
 
     }
 }
